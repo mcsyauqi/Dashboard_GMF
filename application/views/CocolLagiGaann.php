0377a3e2f2876f2d2b006730507d0da447e1acc6
Nuwas Dzarrin Tantowi <nuwasdzarrin3@gmail.com>
2018-09-04 14:50:23 +0700

    Cocol Kedua Kali Gaann

---
+++ application/views/CocolLagiGaann.php
@@ -0,0 +1,8 @@
+KP GMF
+-Surat Bedakan Logo Garuda dan citilink
+-form Threshold and Repetitive
+*role di perbaiki==siap
+*arsip sama all data pilih salah satu==siap
+=menu edit user belum bisa
++role member dapat nyetak.==siap
++yang dapat  upload dan link cuman role admin==siap
